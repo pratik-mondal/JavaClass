e73d5775e15bd8d75a1dc5bb82f697aa8a766e3b
pratik-mondal <pratikmondal274@gmail.com>
2025-07-25 00:57:11 +0530

    new code upload

---
+++ CONCEPT/4 Operators if-else & Number System/Assignment/src/Assignment.java
@@ -0,0 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Assignment {
+    public static void main(String[] args) {
+    int myInt = 9;
+        System.out.println(myInt);
+        int newInt = myInt;
+        System.out.println(newInt);
+    }
+}
