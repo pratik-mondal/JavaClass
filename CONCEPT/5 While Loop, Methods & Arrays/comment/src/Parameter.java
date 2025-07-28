10cb31d8d0e48e9382ecf915552b45f5ee5a07e4
pratik-mondal <pratikmondal274@gmail.com>
2025-07-28 11:31:12 +0530

    new code upload

--- CONCEPT/5 While Loop, Methods & Arrays/comment/src/Parameter.java
+++ CONCEPT/5 While Loop, Methods & Arrays/comment/src/Parameter.java
@@ -1,4 +1,30 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
-public class Parametre {
+public class Parameter {
+
+    public static void main(String[] args) {
+        greet();
+       int result =calculator();
+        System.out.println("the result is : "+result);
+    }
+
+
+    public static void greet(){
+        System.out.print("WELLCOME TO YOUR CALCULATOR\n");                    // greet code
+    }
+
+
+    public static int integer1(){
+        Scanner input = new Scanner(System.in);                               // input code
+        System.out.print("please enter the number : ");
+        int num =  input.nextInt();
+        return num;
+    }
+
+    public static int calculator(){
+       int cal =integer1()+integer1();                                         // result part
+       return cal;
+
+    }
+
 }
